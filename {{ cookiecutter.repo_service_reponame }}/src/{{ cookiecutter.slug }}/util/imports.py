832c5cd9dca6851d034eb446b21bade966417cc0
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-05-11 12:04:04 -0500

    feat: Add module util

--- {{ cookiecutter.repo_service_reponame }}/src/{{ cookiecutter.slug }}/util/imports.py
+++ {{ cookiecutter.repo_service_reponame }}/src/{{ cookiecutter.slug }}/util/imports.py
@@ -1,14 +1,14 @@
 class HandlerRegistry(dict):
     def __missing__(self, name):
-        if "." not in name:
+        if '.' not in name:
             handler = __import__(name)
         else:
-            module_name, handler_name = name.rsplit(".", 1)
+            module_name, handler_name = name.rsplit('.', 1)
 
             module  = __import__(module_name, {}, {}, [handler_name])
             handler = getattr(module, handler_name)
 
-            self[name] = handler
+        self[name] = handler
 
         return handler
 
