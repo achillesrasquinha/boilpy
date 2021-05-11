f362f13397b83a6e90e98d862338e6daab9e196c
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-05-11 12:18:22 -0500

    feat: Add module slug

--- {{ cookiecutter.repo_service_reponame }}/src/{{ cookiecutter.slug }}/exception.py
+++ {{ cookiecutter.repo_service_reponame }}/src/{{ cookiecutter.slug }}/exception.py
@@ -1,1 +1,11 @@
-# Custom exceptions go here.
+# imports - standard imports
+import subprocess as sp
+
+class {{ cookiecutter.slug.capitalize() }}Error(Exception):
+    pass
+
+class PopenError({{ cookiecutter.slug.capitalize() }}Error, sp.CalledProcessError):
+    pass
+
+class DependencyNotFoundError(ImportError):
+    pass
