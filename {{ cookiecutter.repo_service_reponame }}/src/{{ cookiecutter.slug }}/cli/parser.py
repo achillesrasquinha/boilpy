c2c07719082b962de28fe907677264a5f747f9e9
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-12-02 08:57:57 -0600

    feat: update

--- {{ cookiecutter.repo_service_reponame }}/src/{{ cookiecutter.slug }}/cli/parser.py
+++ {{ cookiecutter.repo_service_reponame }}/src/{{ cookiecutter.slug }}/cli/parser.py
@@ -1,6 +1,5 @@
 # imports - standard imports
 import sys
-import os
 import argparse
 import multiprocessing as mp
 
--- {{ cookiecutter.repo_service_reponame }}/src/{{ cookiecutter.slug }}/exception.py
+++ {{ cookiecutter.repo_service_reponame }}/src/{{ cookiecutter.slug }}/exception.py
@@ -1,10 +1,4 @@
-# imports - standard imports
-import subprocess as sp
-
 class {{ cookiecutter.slug.capitalize() }}Error(Exception):
-    pass
-
-class PopenError({{ cookiecutter.slug.capitalize() }}Error, sp.CalledProcessError):
     pass
 
 class DependencyNotFoundError(ImportError):
